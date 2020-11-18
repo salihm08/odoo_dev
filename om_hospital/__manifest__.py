004cec7b4e3188038bcab1eda7478587c404d6af
salihm08 <salihm08@gmail.com>
2020-11-18 09:04:50 +0300

    [ADD] initial commit for OM Hospital

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Hospital Management',
+    'version' : '12.0.1.0.0',
+    'summary': 'Module for managing the hospital',
+    'sequence': '15',
+    'author' : 'salih',
+    'depends' : ['base'],
+    'data' : [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+
+}
